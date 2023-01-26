48f2a8ab7d27ab1e23cefb9b861c611391777d88
taroeld <tarob@live.de>
2023-01-26 14:07:08 +0100

    docs: improve movezip help

--- ConfigCommand.cs
+++ ConfigCommand.cs
@@ -22,7 +22,7 @@ namespace BBbuilder
                 {"modpath", "Set path to the mods directory. (For example: bbuilder config modpath G:/Games/BB/Mods/WIP)" },
                 {"folders", "Add folders to be included in projects. Must be space. New mods added by the init command are automatically added." +
                 " (For example: bbuilder config folders G:/Games/BB/Mods/WIP/mod_msu F:/MODDING/basegame/scripts)"},
-                {"movezip", "Whether you want the final .zip file to be copied or moved to data. Default is copy." }
+                {"movezip", "Whether you want the final .zip file to be copied or moved to data. Default is 'false' (copy). Pass 'true' to move instead." }
             };
         }
 
